4f6e02b728a911fe41757186493ad5f00b9ec5f6
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-05-30 19:06:35 -0300

    hr worker implementation

---
+++ hr-worker/src/main/java/com/br/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.br.hrworker.repositories;
+
+import com.br.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker,Long> {
+}
